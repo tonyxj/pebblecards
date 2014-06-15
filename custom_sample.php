a82b63e33074a28b5c1f22681675f3cf97762cff
Keanu Lee <keanulee517@gmail.com>
2014-06-15 13:19:07 -0400

    New design

--- custom_sample.php
+++ custom_sample.php
@@ -1,8 +1,18 @@
 <?php
 
+$content = "Hello ".$_REQUEST['name'].",\nWelcome to Pebble Cards!";
+
+if ($_REQUEST['latitude'] && $_REQUEST['longitude']) {
+  $content .= "\n\nLat: ".substr($_REQUEST['latitude'], 0, 8)."\nLon: ".substr($_REQUEST['longitude'], 0, 8);
+}
+
 $array = array(
-  "content" => "Hello ".$_REQUEST['name'].",\nWelcome to Pebble Cards!",
+  "content" => $content,
   "refresh_frequency" => 30
 );
+
+if ($_REQUEST['vibrate']) {
+  $array["vibrate"] = $_REQUEST['vibrate'];
+}
 
 echo json_encode($array);
